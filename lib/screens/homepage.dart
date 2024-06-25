f67cf8cb3510203051cef49d017949821be663b1
Victor Cuerdo <victorcuerdop@gmail.com>
2024-06-24 23:37:09 -0500

    added a tool-menu next to the appbar title with navigation options

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:drawing_app/widgets/menu.dart' as custom_widgets;
+import 'package:drawing_app/widgets/tools_menu.dart' as tools_widgets;
 import '../widgets/settings_button.dart';
 import '../canvas/canvas_logic.dart';
 
@@ -12,51 +14,107 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         toolbarHeight: MediaQuery.of(context).size.height * 0.07,
         toolbarOpacity: 0.7,
-        title: RichText(
-          text: const TextSpan(
-            children: [
-              TextSpan(
-                text: 'QUOTE: ',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Color(0xFFEEF4ED),
-                ),
+        title: Row(
+          children: [
+            RichText(
+              text: const TextSpan(
+                children: [
+                  WidgetSpan(
+                    child: AutoSizeText(
+                      
+                      'QUOTE: ',
+                      style: TextStyle(
+                      
+                        //fontSize: 18,
+                        color: Color(0xFFEEF4ED),
+                      ),
+                      maxLines: 1,
+                      minFontSize: 8,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: AutoSizeText(
+                      'Fitzgerald Remodel',
+                      style: TextStyle(
+                        //fontSize: 18,
+                        color: Colors.white,
+                      ),
+                      maxLines: 1,
+                      minFontSize: 8,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
               ),
-              TextSpan(
-                text: 'Fitzgerald Remodel',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: tools_widgets.ToolsMenu(
+                buttons: [
+                  tools_widgets.ToolButton(
+                    icon: Icons.help,
+                    title: 'Help',
+                    onTap: () {
+                      // Handle Tool 1 tap
+                    },
+                  ),
+                  tools_widgets.ToolButton(
+                    icon: Icons.undo,
+                    title: 'Undo',
+                    onTap: () {
+                      // Handle Tool 2 tap
+                    },
+                  ),
+                  tools_widgets.ToolButton(
+                    icon: Icons.save,
+                    title: 'Save',
+                    onTap: () {
+                      // Handle Tool 3 tap
+                    },
+                  ),
+                  tools_widgets.ToolButton(
+                    icon: Icons.close,
+                    title: 'Exit',
+                    onTap: () {
+                      // Handle Tool 4 tap
+                    },
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
         backgroundColor: const Color(0xFF5B9851),
       ),
+      
       body: Column(
         children: [
+          const Divider(
+            height: 1,
+            color: Colors.white,
+          ),
           custom_widgets.MenuBar(
             buttons: [
               custom_widgets.MenuButton(
                 icon: Icons.crop_5_4,
                 title: 'New Kitchen Countertop',
                 onTap: () {
-               
+                  // Handle New Kitchen Countertop tap
                 },
               ),
               custom_widgets.MenuButton(
                 icon: Icons.crop_3_2,
                 title: 'New Island',
                 onTap: () {
-                 
+                  // Handle New Island tap
                 },
               ),
               custom_widgets.MenuButton(
                 icon: Icons.download,
                 title: 'Export to DXF file',
                 onTap: () {
-              
+                  // Handle Export to DXF file tap
                 },
               ),
             ],
---
+++ lib/widgets/tools_menu.dart
@@ -0,0 +1,90 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+class ToolsMenu extends StatefulWidget {
+  final List<ToolButton> buttons;
+
+  const ToolsMenu({super.key, required this.buttons});
+
+  @override
+  _ToolsMenuState createState() => _ToolsMenuState();
+}
+
+class _ToolsMenuState extends State<ToolsMenu> {
+  int? _activeIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      color: const Color(0xFF5B9851),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start, 
+        children: widget.buttons.asMap().entries.map((entry) {
+          int index = entry.key;
+          ToolButton button = entry.value;
+          return _buildToolButton(
+            context,
+            button.icon,
+            button.title,
+            button.onTap,
+            index,
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _buildToolButton(
+    BuildContext context,
+    IconData icon,
+    String title,
+    Function onTap,
+    int index,
+  ) {
+    bool isActive = _activeIndex == index;
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _activeIndex = index;
+        });
+        onTap();
+        Future.delayed(const Duration(milliseconds: 300), () {
+          setState(() {
+            _activeIndex = null;
+          });
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 3.5), 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: isActive ? Colors.greenAccent : Colors.white, size: 24),
+            const SizedBox(height: 3),
+            AutoSizeText(
+              title,
+              style: TextStyle(
+                color: isActive ? Colors.greenAccent : Colors.white,
+                fontSize: 12,
+              ),
+              maxLines: 1,
+              minFontSize: 8,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ToolButton {
+  final IconData icon;
+  final String title;
+  final Function onTap;
+
+  ToolButton({required this.icon, required this.title, required this.onTap});
+}
